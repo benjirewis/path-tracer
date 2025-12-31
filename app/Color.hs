c2f9ee6eb3c318c34cb55cf1e792f0b83a9cfbda
Benjamin Rewis <benji.rewis@viam.com>
2025-12-31 17:10:26 -0500

    Make writeColor more functional

--- app/Color.hs
+++ app/Color.hs
@@ -5,7 +5,6 @@ module Color
 where
 
 import System.IO (Handle, hPutStrLn)
-import Text.Printf (printf)
 import Vector (Vec3 (..))
 
 color :: Double -> Double -> Double -> Vec3
@@ -16,9 +15,7 @@ upperLimit = 255.999
 
 writeColor :: Handle -> Vec3 -> IO ()
 writeColor handle (Vec3 r g b) = do
-  -- Translate the [0,1] component values to the byte range [0,255].
-  let rInt = floor (upperLimit * r) :: Int
-  let gInt = floor (upperLimit * g) :: Int
-  let bInt = floor (upperLimit * b) :: Int
-  -- Write color components to handle.
-  hPutStrLn handle $ printf "%d %d %d" rInt gInt bInt
+  -- Translate the [0,1] component values to the byte range [0,255] and write them to
+  -- handle.
+  let toInt = floor . (* upperLimit) :: Double -> Int
+  hPutStrLn handle $ unwords $ map (show . toInt) [r, g, b]
